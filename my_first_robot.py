ca4b2bb78a66f53288ed299891cb6c90e77911f7
Fard <fardilizer@gmail.com>
2026-07-22 09:51:14 -0700

    Implement connection check for MrSparky
    Add connection check for MrSparky robot

--- my_first_robot.py
+++ my_first_robot.py
@@ -3,3 +3,10 @@ from sparkybotmini import SparkyBotMini
 
 mr_sparky = SparkyBotMini(port = "/dev/ttyUSB0")
 
+if mr_sparky.connect():
+  print("MrSparky has been connected")
+else:
+  print("problem connecting to MrSparky")
+  exit()
+
+mr_sparky.disconnect()
