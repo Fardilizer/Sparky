c8badb1b0874315e3af3e3052d8ddacbfbdeed2b
Fard <fardilizer@gmail.com>
2026-07-22 09:52:20 -0700

    Make robot beep upon successful connection
    Add a beep sound after connecting to the robot

--- my_first_robot.py
+++ my_first_robot.py
@@ -5,6 +5,8 @@ mr_sparky = SparkyBotMini(port = "/dev/ttyUSB0")
 
 if mr_sparky.connect():
   print("MrSparky has been connected")
+  robot.beep(300)
+  time.sleep(1)
 else:
   print("problem connecting to MrSparky")
   exit()
