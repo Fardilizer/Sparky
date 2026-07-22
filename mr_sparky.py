445e395a8200da13b15081842a9af02f5f8c91e8
Fard <fardilizer@gmail.com>
2026-07-22 10:06:10 -0700

    Update mr_sparky.py

--- mr_sparky.py
+++ mr_sparky.py
@@ -3,6 +3,20 @@ from sparkybotmini import SparkyBotMini
 
 mr_sparky = SparkyBotMini(port = "/dev/ttyUSB0")
 
+def move_function(dir, power, time):
+  if dir == "forward":
+    mr_sparky.set_motor(power, power, power, power)
+  elif dir == "backward":
+    mr_sparky.set_motor(-power, -power, -power, -power)
+  elif dir == "right":
+    mr_sparky.set_motor(power, -power, power, -power)
+  elif dir == "left":
+    mr_sparky.set_motor(-power, power, -power, power)
+  elif dir == "t_left":
+    mr_sparky.set_motor(power, power -power, -power)
+  elif dir == "t_right":
+    mr_sparky.set_motor(-power, -power, power, power)
+
 def main_function():
   mr_sparky.set_motor(-40, 40, 40, -40)
 
